38f9d103451fbad89e5eaf226c897725d2cc1959
Kayla Plunkett <kayla.lee.plunkett@gmail.com>
2024-08-02 22:44:40 -0500

    Begin react conversion

---
+++ src/clues/C1.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+const C1 = () => {
+
+  return(
+    <div className='clue'>
+		<div className='w2'>
+			<span className='letter'>I</span>
+			<span className='letter'>t</span>
+			<span className='letter'>s</span>
+			<span className='letter'> </span>
+			<span className='letter'>a</span>
+			<span className='letter'>n</span>
+			<span className='letter'> </span>
+			<span className='letter'>A</span>
+			<span className='letter'>n</span>
+			<span className='letter'>a</span>
+			<span className='letter'>g</span>
+			<span className='letter'>r</span>
+			<span className='letter'>a</span>
+			<span className='letter'>m</span>
+		</div>
+		
+		<div className='w1'>
+			<span className='letter'>S</span>
+			<span className='letter'>a</span>
+			<span className='letter'>t</span>
+			<span className='letter'>i</span>
+			<span className='letter'>n</span>
+			<span className='letter'> </span>
+			<span className='letter'>R</span>
+			<span className='letter'>a</span>
+			<span className='letter'>g</span>
+			<span className='letter'>a</span>
+			<span className='letter'> </span>
+			<span className='letter'>M</span>
+			<span className='letter'>a</span>
+			<span className='letter'>n</span>
+		</div>
+		
+		<button className='show-btn'>Solve</button>
+
+    </div>
+  )
+}
+
+export default C1;
---
+++ src/components/TopBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TopBar = () => {
+
+  return(
+    <div class='top-bar'>
+		<p>Learn Cryptic</p>
+    </div>
+  )
+}
+
+export default TopBar;
