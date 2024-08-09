e6bd3450729cf177cbf072f7dbb2c0bb6ff975b5
Kayla Plunkett <kayla.lee.plunkett@gmail.com>
2024-08-09 12:18:44 -0500

    Separate hooks. Clean activeClue.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-import React, { useState, useEffect, useRef, act } from 'react'
-import ClueContainer from './components/ClueContainer'
+import React from 'react'
+
+// components
 import TopBar from './components/TopBar'
+import ClueContainer from './components/clueContainer/ClueContainer'
 import Bottom from './components/bottom/Bottom'
 
-import clues from './clues.json';
+// hooks
+import useActiveClue from './hooks/useActiveClue'
 
 const App = () => {
-
-	// state
-	const [solution, setSolution] = useState(false);
-	const [activeClue, setActiveClue] = useState(clues[3]);
-
-	// get solution letters
-	const getSolutionLetters = solution => `(${solution.split(' ').map(word => word.length).join(' ')})`
-	activeClue.solutionLetters = getSolutionLetters(activeClue.solution)
-
-	console.log(activeClue);
+	let { activeClue, solution, setSolution } = useActiveClue()	
 
     return (
 		<>
