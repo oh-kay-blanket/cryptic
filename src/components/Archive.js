7c4217f64db1518b5ac14235cad6e7041ac15b00
Kayla Plunkett <kayla.lee.plunkett@gmail.com>
2024-09-07 19:49:10 -0500

    Improve archive tile

--- src/components/Archive.js
+++ src/components/Archive.js
@@ -1,6 +1,12 @@
 import React, { useRef, createRef, useEffect } from 'react'
 
+import d1 from '../img/difficulty/1.svg';
+import d2 from '../img/difficulty/2.svg';
+import d3 from '../img/difficulty/3.svg';
+import d4 from '../img/difficulty/4.svg';
+
 const Archive = ({ clues, setclueId, setMode, completedClues, setInput, setCheckAns }) => {
+
 
 	let tilesRef = useRef(clues.map(() => createRef()))
 
@@ -13,12 +19,39 @@ const Archive = ({ clues, setclueId, setMode, completedClues, setInput, setCheck
 
 	const archiveTiles = clues.map((clue, index) => {
 
+		const getRelease = (release) => {
+
+		}
+
+		const getImg = (difficulty) => {
+			switch (Number(difficulty)) {
+				case 1:
+				  return d1
+				case 2:
+					return d2
+				case 3:
+					return d3
+				case 4:
+					return d4
+				default:
+					return d1
+			  }
+		}
+
 		const isComplete = completedClues.includes(Number(clue.id)) ? ' completed' : ''
 
-		return <div key={clue.id} id={clue.id} className={`archive-tile${isComplete}`} ref={tilesRef.current[index]} onClick={()=>handleClick(tilesRef.current[index])}>
-			<span className='tile-name'>{clue.clue.value}</span>
-			<span className='tile-source'>{clue.source}</span>
+		return (
+		<div className='archive-clue' key={clue.id}>
+			<div className='archive-release'>{clue.release}</div>
+			<div id={clue.id} className={`archive-tile${isComplete}`} ref={tilesRef.current[index]} onClick={()=>handleClick(tilesRef.current[index])}>
+				<span className='tile-name'>{clue.clue.value}</span>
+				<div className='tile-info'>
+					<img className='tile-difficulty' src={getImg(clue.difficulty)} />
+					<span className='tile-source'>{clue.source}</span>
+				</div>
+			</div>
 		</div>
+		)
 	})
 
 	return(
