30de9b55029de59e913794af657f840fb0189514
Kayla Plunkett <kayla.lee.plunkett@gmail.com>
2024-09-19 22:44:16 -0500

    Begin placement

---
+++ src/utils/clue/getLines.js
@@ -0,0 +1,16 @@
+const getLines = (letters) => {
+
+	const difference = Math.abs(letters[0].current.getBoundingClientRect().top - letters[letters.length -1].current.getBoundingClientRect().top)
+	switch(true) {
+		case (difference == 0):
+			return 1
+		case (difference > 1 && difference <= 35):
+			return 2
+		case (difference > 35 && difference <= 55):
+			return 3
+		case (difference > 55):
+			return 4
+	}
+}
+
+export default getLines
