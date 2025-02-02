5685febc2cf3946718ee13c71d1013a6f09b47f1
Kayla Plunkett <kayla.lee.plunkett@gmail.com>
2025-02-02 16:21:20 -0600

    Letter bank & spoonerism

--- src/components/bottom/Message.js
+++ src/components/bottom/Message.js
@@ -35,6 +35,10 @@ const Message = ({ setShowMessage, activeClue, setclueId, nextHint, setNextHint,
 						return <><strong>{hint.value.toUpperCase()}</strong>, indicates a container</>
 					case 'deletion':
 						return <><strong>{hint.value.toUpperCase()}</strong>, indicates a deletion</>
+					case 'delete even':
+						return <><strong>{hint.value.toUpperCase()}</strong>, indicates deleting the even letters from <strong>{hint.end.value[0].toUpperCase()}</strong></>
+					case 'delete odd':
+						return <><strong>{hint.value.toUpperCase()}</strong>, indicates deleting the odd letters from <strong>{hint.end.value[0].toUpperCase()}</strong></>
 					case 'direct':
 						return <><strong>{hint.value.toUpperCase()}</strong> is used</>
 					case 'hidden word':
@@ -43,10 +47,14 @@ const Message = ({ setShowMessage, activeClue, setclueId, nextHint, setNextHint,
 						return <><strong>{hint.end.value[1].toUpperCase()}</strong> is hidden within <strong>{hint.end.value[0].toUpperCase()}</strong></>
 					case 'homophone':
 						return <><strong>{hint.value.toUpperCase()}</strong> indicates a homophone</>
+					case 'hp-2':
+						return <><strong>{hint.end.value[1].toUpperCase()}</strong> is a homophone of <strong>{hint.end.value[0].toUpperCase()}</strong></>
 					case 'initialism':
 						return <><strong>{hint.value.toUpperCase()}</strong> indicates the beginning of one or more words</>
 					case 'letter bank':
 						return <><strong>{hint.value.toUpperCase()}</strong> indicates a letter bank</>
+					case 'lb-2':
+						return <><strong>{hint.end.value[0].toUpperCase()}</strong> is an letter bank for <strong>{hint.end.value[1].toUpperCase()}</strong></>
 					case 'particle':
 						return <><strong>{hint.value.toUpperCase()}</strong> can be <strong>{hint.end.value[0].toUpperCase()}</strong></>
 					case 'reversal':
@@ -55,6 +63,8 @@ const Message = ({ setShowMessage, activeClue, setclueId, nextHint, setNextHint,
 						return <><strong>{hint.value.toUpperCase()}</strong> can be <strong>{hint.end.value[0].toUpperCase()}</strong></>
 					case 'symbol':
 						return <><strong>{hint.value.toUpperCase()}</strong> can be <strong>{hint.end.value[0].toUpperCase()}</strong></>
+					case 'spoonerism':
+						return <><strong>{hint.end.value[1].toUpperCase()}</strong> is a spoonerism of <strong>{hint.end.value[0].toUpperCase()}</strong></>
 					case 'dd-2':
 						return
 					default:
--- src/utils/clue/addLetters.js
+++ src/utils/clue/addLetters.js
@@ -11,31 +11,49 @@ const addLetters = (activeClue, hint) => {
 		switch(hint.category) {
 			case 'anagram':
 			case 'hidden word':
+			case 'letter bank':
 			case 'particle':
+			case 'synonym':
+			case 'symbol':
 				hint.addLetters.value = hint.end.value[0].split('')
 				break
-			case 'hw-2':
-				hint.addLetters.value = removeSpecial(hint.end.value[1]).split('')
-				break
-			case 'letter bank':
-				hint.addLetters.value = [...hint.end.value[1].split(''), ...hint.end.value[1].split('')]
-				break
+
 			case 'container':
 				hint.addLetters.value = [...hint.end.value.join("").split(''), ...hint.end.value.join(" ").split('')]
 				break
+
+			case 'lb-2':
+				hint.addLetters.value = [...hint.end.value[1], ...hint.end.value[1]]
+				break
+
 			case 'direct':
 				hint.addLetters.value = hint.value.split('')
 				break
+
 			case 'homophone':
 				hint.addLetters.value = ['= ', activeClue.solution.value]
 				break
+
+			case 'hw-2':
+				hint.addLetters.value = removeSpecial(hint.end.value[1]).split('')
+				break
+
 			case 'initialism':
 				hint.addLetters.value = hint.end.value.map(endPt => {
 					return endPt.split(' ').map(word => word.split('')[0]).flat()
 				}).flat()
 				break
-			default:
+
+			case 'ag-2':
+			case 'reversal':
 				hint.addLetters.value = hint.end.value.map(endPt => endPt.split('')).flat()
+				break
+
+			case 'spoonerism':
+				hint.addLetters.value = hint.end.value[1].split('')
+				break
+
+			default:
 				break
 		}
 
