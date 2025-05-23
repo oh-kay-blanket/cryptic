0ad25afe190f164a59346f4701f171199e3932d5
Kayla Plunkett <kayla.lee.plunkett@gmail.com>
2025-05-22 23:24:29 -0500

    Clean up learn

--- src/pages/Learn.js
+++ src/pages/Learn.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect } from 'react'
-import { Link } from "gatsby"
+import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 import ButtonContainer from '../components/bottom/ButtonContainer'
@@ -11,19 +11,18 @@ import showHint from '../assets/img/learn/show-hint.png'
 import ex from '../assets/img/learn/example.jpg'
 
 const Learn = () => {
-
 	const { typeViewed } = useContext(UserContext)
 
 	// buttons
 	const buttons = {
 		easyClue: {
 			path: '/clues/208',
-			name: "Try a clue",
-			style: 'primary'
-		}
+			name: 'Try a clue',
+			style: 'primary',
+		},
 	}
 	const btnArr = [buttons.easyClue]
-	
+
 	const typesArr = [
 		{ name: 'Anagram', id: 'anagram' },
 		{ name: 'Charade', id: 'charade' },
@@ -37,67 +36,96 @@ const Learn = () => {
 		{ name: 'Reversal', id: 'reversal' },
 		{ name: 'Spoonerism', id: 'spoonerism' },
 		{ name: '& Lit.', id: 'lit' },
-		{ name: 'Combination', id: 'combination' }
+		{ name: 'Combination', id: 'combination' },
 	]
 
-	const types = typesArr.map(type => {
-		const isViewed = typeViewed.find(viewed => viewed === type.id)
-		return <li key={type.id} className={isViewed ? 'viewed' : ''}><Link to={type.id} >{type.name}</Link></li>
+	const types = typesArr.map((type) => {
+		const isViewed = typeViewed.find((viewed) => viewed === type.id)
+		return (
+			<li key={type.id} className={isViewed ? 'viewed' : ''}>
+				<Link to={type.id}>{type.name}</Link>
+			</li>
+		)
 	})
 
 	// Handle anchor link
 	useEffect(() => {
 		if (typeof window !== 'undefined') {
-		  const hash = window.location.hash
-		  if (hash) {
-			// slight delay ensures element is present
-			setTimeout(() => {
-			  const el = document.querySelector(hash)
-			  if (el) {
-				el.scrollIntoView({ behavior: 'instant' })
-			  }
-			}, 1)
-		  }
+			const hash = window.location.hash
+			if (hash) {
+				// slight delay ensures element is present
+				setTimeout(() => {
+					const el = document.querySelector(hash)
+					if (el) {
+						el.scrollIntoView({ behavior: 'instant' })
+					}
+				}, 1)
+			}
 		}
-	  }, [])
+	}, [])
 
 	return (
 		<Layout>
 			<div className='learn container'>
-
 				<div className='learn-section'>
 					<h2 className='learn-question'>What is a cryptic crossword?</h2>
-					<p className='learn-answer'>A cryptic crossword is a type of crossword in which each clue is a puzzle in itself, often involving wordplay, anagrams, homophones, hidden words, or other linguistic tricks. Unlike standard crosswords, where clues are straightforward definitions, cryptic clues typically have two parts:</p>
+					<p className='learn-answer'>
+						A “cryptic,” or “British-style” crossword is one in which each clue
+						is a puzzle in itself, involving wordplay such as anagrams,
+						homophones, hidden words, and other devilish tricks.
+						<br />
+						Each clue reads as a sentence, but it’s important to recognize that
+						the meaning of the sentence—its “surface reading”—is not important.
+						Rather, the clue hints at the answer in two different ways, and thus
+						is composed of two parts:
+					</p>
 					<ul className='no-dec'>
 						<li className='mt-3'>
-							<p><strong>Definition</strong></p>
-							<p>A straight or slightly disguised definition of the answer. This will <span className='underline'>always</span> be at the start or end of the clue.</p>
+							<p>
+								<strong>Definition</strong>
+							</p>
+							<p>
+								The rest of the clue is a sort of enigmatic hint, in which
+								wordplay gives a second path to the answer.
+							</p>
 						</li>
 						<li className='mt-3'>
-							<p><strong>Wordplay</strong></p>
-							<p>A cryptic hint involving anagrams, reversals, hidden words, homophones, or other forms of word manipulation.</p>
+							<p>
+								<strong>Wordplay</strong>
+							</p>
+							<p>
+								A cryptic hint involving anagrams, reversals, hidden words,
+								homophones, or other forms of word manipulation.
+							</p>
 						</li>
 					</ul>
-					<p><strong>Example</strong></p>
+					<p>
+						<strong>Example</strong>
+					</p>
 					<div className='example-container'>
-						<div className='example'><img src={ex} alt=""/></div>
+						<p className='example'>Wild West goulash (4)</p>
 						<div className='explanation'>
 							<ul className='mt-0'>
-								<li>The definition is <strong>poison fungus</strong></li>
-								<li><strong>toads</strong> is a synonym for <strong>frogs</strong></li>
-								<li><strong>tool</strong> is a synonym for <strong>saw</strong></li>
+								<li>
+									The definition part is <strong>GOULASH</strong>
+								</li>
+								<li>
+									<strong>WILD</strong> indicates an anagram—picture the letters
+									in the word <strong>WEST</strong> “going wild”
+								</li>
+								<li>
+									<strong>WEST</strong> anagrams to <strong>STEW</strong>,
+									defined by <strong>GOULASH</strong>
+								</li>
 							</ul>
-							<p className='text-center'><strong>toads</strong> + <strong>tool</strong> = <strong>toadstool</strong></p>
+							<p className='text-center'>
+								<strong>west</strong> → <strong>stew</strong>
+							</p>
 							<div className='solution'>
-								<span className='letter'>t</span>
-								<span className='letter'>o</span>
-								<span className='letter'>a</span>
-								<span className='letter'>d</span>
 								<span className='letter'>s</span>
 								<span className='letter'>t</span>
-								<span className='letter'>o</span>
-								<span className='letter'>o</span>
-								<span className='letter'>l</span>
+								<span className='letter'>e</span>
+								<span className='letter'>w</span>
 							</div>
 						</div>
 					</div>
@@ -105,39 +133,79 @@ const Learn = () => {
 
 				<div className='learn-section'>
 					<h2 className='learn-question'>What is Learn Cryptic?</h2>
-					<p className='learn-answer'>Learn Cryptic is a  daily game to help you learn about and practice cryptic clues. There are two ways you can get help with solving clues:</p>
+					<p className='learn-answer'>
+						Learn Cryptic is a daily game that teaches the cryptic-curious how
+						to decipher clues.
+						<br />
+						As with regular American-style crosswords, it takes a lot of
+						practice to get good at solving cryptic puzzles. For beginners, the
+						learning curve can be steep. To help guide you to the answer, the
+						app has two kinds of hints:
+					</p>
 					<ul className='no-dec'>
 						<li className='mt-3'>
-							<p><strong>Wordplay used</strong></p>
-							<p>At the top of each clue you will see purple pills indicating the type(s) of wordplay used in this clue. Tapping on these will reveal more information about that type.</p>
-							<img className='border' src={typePill} alt="" />
+							<p>
+								<strong>Clue type</strong>
+							</p>
+							<p>
+								At the top left of the screen, purple pills tell you the type(s)
+								of wordplay used. A single clue can use as many as four
+								different types of wordplay! Hover over or tap the pills to see
+								a brief description and example of each type.
+							</p>
+							<img className='border' src={typePill} alt='' />
 						</li>
 						<li className='mt-3'>
-							<p><strong>Hints</strong></p>
-							<p>The "Show hint" and "Reveal solution" buttons in the clue provide direct guidance as to what steps you will need to take to find the solution.</p>
-							<img className='border' src={showHint} alt="" />
+							<p>
+								<strong>Hints</strong>
+							</p>
+							<p>
+								The "Show hint" and "Reveal solution" buttons guide you through
+								each step of deciphering the clue.
+							</p>
+							<img className='border' src={showHint} alt='' />
 						</li>
 					</ul>
 				</div>
 
 				<div id='learn-types' className='learn-section'>
-					<h2 className='learn-question'>What different types of wordplay will I find?</h2>
+					<h2 className='learn-question'>
+						What different types of wordplay will I find?
+					</h2>
 					<p className='learn-answer'>Tap a type below to learn more:</p>
 					<ul className='learn-types no-dec'>{types}</ul>
 				</div>
 
 				<div className='learn-section'>
 					<h2 className='learn-question'>Anything else I need to know?</h2>
-					<p className='learn-answer'>Now that you understand the basics of how cryptic clues are constructed and are familiar with some of the different types of hints, dive in and try a few!</p>
-					<ButtonContainer
-						btnArr={btnArr}
-						stack={true}
-					/>
+					<ul className='learn-answer'>
+						<li>
+							A new clue is released each day. Access past clues from the main
+							menu via the “See all clues” button.
+						</li>
+						<li>
+							For each day’s clue, the app keeps track of how many guesses it
+							took you to find the solution, and how many hints you used.{' '}
+						</li>
+						<li>
+							Tap the eye icon in the top left to hide or reveal the purple
+							pills that indicate the clue type(s).
+						</li>
+						<li>
+							If you’ve correctly guessed the answer but you’re not sure how the
+							clue works, press the “Show logic” button to be guided through the
+							steps.
+						</li>
+						<li>
+							Share your results with friends using the “Share score” button.
+						</li>
+					</ul>
+					<p>Now let’s dive in and try a few clues!</p>
+					<ButtonContainer btnArr={btnArr} stack={true} />
 				</div>
 			</div>
 		</Layout>
 	)
 }
-
 
 export default Learn
