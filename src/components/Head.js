b528bf39fe0f24681acc714df7148a3a0981ac3c
Kay Plunkett <kayla.lee.plunkett@gmail.com>
2026-02-17 14:48:04 -0600

    Update theme color

--- src/components/Head.js
+++ src/components/Head.js
@@ -48,7 +48,7 @@ export const Head = ({ title, description, pathname, children }) => {
 			<link rel="apple-touch-icon" href="/favicon.png" />
 			
 			{/* Theme Color */}
-			<meta name="theme-color" content="#faf9f6" />
+			<meta name="theme-color" content="#E1D8FF" />
 			<meta name="msapplication-TileColor" content="#faf9f6" />
 			
 			{/* Additional structured data */}
