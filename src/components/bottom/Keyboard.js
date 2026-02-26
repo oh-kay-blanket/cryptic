1b7e1ab9d4a4d5061b57a9da8fc7aba0f3d65ef9
Kay Plunkett <kayla.lee.plunkett@gmail.com>
2026-02-26 10:08:18 -0600

    Handle keyboard sizing

--- src/components/bottom/Keyboard.js
+++ src/components/bottom/Keyboard.js
@@ -23,6 +23,13 @@ const Keyboard = ({ handleInput, onEnter, canCheckAnswer }) => {
 	}, [handleInput, onEnter, canCheckAnswer])
 
 	return (
+		<>
+		<div className='rotate-message'>
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+				<path d="M7.5 2.25a.75.75 0 0 0-.75.75v18c0 .414.336.75.75.75h9a.75.75 0 0 0 .75-.75V3a.75.75 0 0 0-.75-.75h-9ZM6.75 3A2.25 2.25 0 0 1 9 .75h6A2.25 2.25 0 0 1 17.25 3v18A2.25 2.25 0 0 1 15 23.25H9A2.25 2.25 0 0 1 6.75 21V3Z" />
+			</svg>
+			<span>Rotate device for keyboard</span>
+		</div>
 		<div className='lc-container keyboard'>
 			<div className='k-row k-row-1'>
 				<button
@@ -231,6 +238,7 @@ const Keyboard = ({ handleInput, onEnter, canCheckAnswer }) => {
 				</button>
 			</div>
 		</div>
+		</>
 	)
 }
 
