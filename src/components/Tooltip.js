2ac1aad9149c4976c8ea1402660ae6f3897516d9
Kay Plunkett <kayla.lee.plunkett@gmail.com>
2026-04-08 22:59:02 -0500

    Fix tooltip links

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,43 +1,43 @@
-import React from 'react'
-import { useStaticQuery, graphql, Link } from 'gatsby'
+import React from "react";
+import { useStaticQuery, graphql, Link } from "gatsby";
 
 // Map type names to URL paths
 const getLearnPath = (type) => {
-	const map = {
-		'& Lit.': 'lit',
-		'Double Definition': 'double-definition',
-		'Hidden Word': 'hidden-word',
-		'Letter Bank': 'letter-bank',
-	}
-	return map[type] || type.toLowerCase()
-}
+  const map = {
+    "& Lit.": "lit",
+    "double definition": "double-definition",
+    "hidden word": "hidden-word",
+    "letter bank": "letter-bank",
+  };
+  return map[type] || type.toLowerCase();
+};
 
 const Tooltip = ({ text }) => {
-	const data = useStaticQuery(graphql`
-		query AllTypeQuery {
-			allTypeJson {
-				nodes {
-					type
-					description
-					example
-				}
-			}
-		}
-	`)
+  const data = useStaticQuery(graphql`
+    query AllTypeQuery {
+      allTypeJson {
+        nodes {
+          type
+          description
+          example
+        }
+      }
+    }
+  `);
 
-	const matchType = data.allTypeJson.nodes.find(
-		(type) => type.type.toUpperCase() === text.toUpperCase()
-	)
+  const matchType = data.allTypeJson.nodes.find(
+    (type) => type.type.toUpperCase() === text.toUpperCase(),
+  );
 
-	return (
-		<div role='tooltip' className='tooltip' id={text}>
-			<p className='tooltip-description'>{matchType.description}</p>
-			<p className='tooltip-example'>{matchType.example}</p>
-			<Link to={`/learn/${getLearnPath(text)}`} className='tooltip-learn-link'>
-				Learn more &rarr;
-			</Link>
-		</div>
-	)
-}
+  return (
+    <div role="tooltip" className="tooltip" id={text}>
+      <p className="tooltip-description">{matchType.description}</p>
+      <p className="tooltip-example">{matchType.example}</p>
+      <Link to={`/learn/${getLearnPath(text)}`} className="tooltip-learn-link">
+        Learn more &rarr;
+      </Link>
+    </div>
+  );
+};
 
-export default Tooltip
+export default Tooltip;
